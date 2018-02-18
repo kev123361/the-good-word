82ae7e6d16a286a182ff24fbed3486c08f2a90cb
Kevin Tang <kev123361@gmail.com>
2018-02-18 17:38:31 -0500

    Firebase connection and functionality

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,6 +12,20 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { PlaceholderInfoPage } from '../pages/placeholder-info/placeholder-info';
 
+//import {Firebase } from '@ionic-native/firebase';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule, AngularFireDatabase} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBiTz1cNiCndam8NFKy0qVhvVGfO1F1Dqw",
+	authDomain: "the-good-word.firebaseapp.com",
+  databaseURL: "https://the-good-word.firebaseio.com",  
+  projectId: "the-good-word",
+  storageBucket: "the-good-word.appspot.com",
+  messagingSenderId: "705962135776"
+}
+
 @NgModule({
   declarations: [
     MyApp,
@@ -24,6 +38,9 @@ import { PlaceholderInfoPage } from '../pages/placeholder-info/placeholder-info'
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -35,6 +52,8 @@ import { PlaceholderInfoPage } from '../pages/placeholder-info/placeholder-info'
     DetailPage
   ],
   providers: [
+    //Firebase,
+    AngularFireDatabase,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- frontend/src/pages/hello-ionic/hello-ionic.ts
+++ frontend/src/pages/hello-ionic/hello-ionic.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import {PlaceholderInfoPage} from '../placeholder-info/placeholder-info';
 import { NavController, NavParams} from 'ionic-angular';
 import {DetailPage} from '../detail/detail';
+//import {Firebase} from '@ionic-native/firebase';
+import {AngularFireDatabase} from 'angularfire2/database';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'page-hello-ionic',
@@ -9,14 +12,34 @@ import {DetailPage} from '../detail/detail';
 })
 export class HelloIonicPage {
   todayItems: any[];
+  dataList: Observable<any[]>;
   
     
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private firebase:AngularFireDatabase) {
     //this.items = createInitialItemList();
+    // this.firebase.getToken()
+    //   .then(token => console.log('The token is ${token}'))
+    //   .catch(error => console.error('Error getting token'));
+    
+    // this.firebase.activateFetched()
+    //   .then(result => {
+    //     console.log(JSON.stringify(result));
+    //     this.firebase.getValue("0")
+    //     .then(data => {
+    //       console.log(data);
+    //     });
+    //   });
+
+    // this.firebase.onTokenRefresh()
+    //   .subscribe((token: string) => console.log('Got a new token ${token}'));
+
+    
     this.todayItems = [];
     this.todayItems.push({ID: 0, name: "Aliens vs. Zombies", details: "some details", time: "11:00PM", location: "COC", source: "https://www.google.com"});
     this.todayItems.push({ID: 1, name: "Volleyball", details: "some details2", time: "13:00PM", location: "CULC", source: "https://www.facebook.com"});
 
+    this.dataList = firebase.list('data').valueChanges();
+    console.log(this.dataList);
     
     
   }
@@ -26,6 +49,7 @@ export class HelloIonicPage {
       item: item
     });
   }*/
+
 
   itemSelected(item) {
     this.navCtrl.push(DetailPage, {item: item});
